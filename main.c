46318a1bbcb504890b533f8e3313b2632de6dfa8
tristanvong <146710405+tristanvong@users.noreply.github.com>
2026-06-09 14:09:55 +0200

    feat: passphrase command can now give one random word
    A lot of features are missing they are written in the comments

--- main.c
+++ main.c
@@ -6,8 +6,9 @@
 
 #include <openssl/rand.h>
 
-#define COMMANDS "coin compare dice discount help lower upper"
+#define COMMANDS "coin compare dice discount help lower passphrase upper"
 #define PROGRAM_NAME "trisutils"
+#define ASCII_NUM_START 48
 
 void print_help()
 {
@@ -143,6 +144,110 @@ int main(int argc, const char *argv[])
             user_input[i] = tolower(user_input[i]);
         }
         printf("\n%s\n", user_input);
+    }
+
+    if (strcmp(command, "passphrase") == 0)
+    {
+        /*  Thanks,
+            cppreference dot com
+            for providing the base of this code under CC-license.
+        */
+        /*  for now fine, should be stored somewhere better
+            I think /usr/local/share/trisutils/eff_large_wordlist.txt is appropriate?
+            Now that I think about it would need root/superuser rights,... maybe better to store it in home folder
+        */
+        const char *file_name = "eff_large_wordlist.txt";
+        FILE* fp = fopen(file_name, "r");
+
+        if (!fp)
+        {
+            perror("File opening failed.");
+            return EXIT_FAILURE;
+        }
+
+        rewind(fp);
+        int c, count, count_word, current_int, found_match, random_int;
+        c = count = count_word = current_int = found_match = random_int = 0;
+        char number_str[5];
+        char number_str_random[5];
+        char word_buffer[128]; /* this is my scuffed approach for now but I want to create a variable that scales see text below */
+
+        for (int i = 0; i < 5; i++)
+        {
+            random_int = (int)random_uint(5) + 1;
+            number_str_random[i] = ASCII_NUM_START + random_int;
+        }
+
+        int generated_int = atoi(number_str_random);
+            
+        while ((c = fgetc(fp)) != EOF)
+        {
+            /*
+                note to self for later:
+
+                File is structered like this:
+                11111   word
+
+                0. generate 5 random numbers
+                1. take 5 numbers from EFF File put in a variable until whitespace
+                2. check whether 5 numbers are equal to generated int from step 0
+                2.1. if not, repeat step 1
+                2.2. else, print each char to terminal
+
+                DONE:
+                    finding one singular word based on 5 generated nums
+                TODO:
+                    instead of printing to the terminal store the word in a variable, this way it is possible to strcpy it to the result variable each time (using strlen)
+                        1. re-create the word variable each time
+                        2. copy this word variable to a result variable that gets bigger each time
+                    implement a way so that multiple words can be generated
+                    implement a divisor variable that sits between words default would be a whitespace " "
+            */
+            
+            if(!found_match && isdigit(c) && !isblank(c) && !isalpha(c) && c != '\n')
+            {
+                number_str[count] = c;
+                count++;
+            } else if (!found_match && isblank(c))
+            {
+                current_int = atoi(number_str);
+
+                if (generated_int == current_int)
+                    found_match = 1;
+
+                count = 0;
+                for (int i = 0; i < strlen(number_str); i++)
+                    number_str[i] = ' ';
+            }
+
+            if (found_match && !isblank(c) && !isdigit(c) && isalpha(c))
+            {
+                /* word being build */
+                printf("%c",c);
+                word_buffer[count_word] = c;
+                count_word++;
+            }
+
+            if (c == '\n' && found_match)
+            {
+                /* somehow put the word in a var and result string see above text for more info */
+                printf("\n");
+                break;
+            }
+        }
+        
+        if (ferror(fp))
+        {
+            puts("I/O error when reading.");
+            return EXIT_FAILURE;
+        } else if (feof(fp))
+        {
+            puts("End of file reached successfully.");
+            return EXIT_SUCCESS;
+        }
+        
+        fclose(fp);
+        return EXIT_SUCCESS;
     }
 
     if (strcmp(command, "upper") == 0)
